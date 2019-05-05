4859e55b09714fb9738d12867f4b7c1d544a3e1a
CH <>
2019-05-05 17:10:49 +0800

    add

--- src/config/router.config.ts
+++ src/config/router.config.ts
@@ -1,11 +1,12 @@
 import { routerItem } from '@/interface';
+import hellow from '../components/HelloWorld.vue';
 
 export const appRouter: routerItem[] = [
   {
     path: '/',
     name: '',
     meta: { title: '首页' },
-    component: () => import('@/views/Home/')
+    component: hellow
   }
 ];
 
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,12 +1,18 @@
 import { Component, Vue, Emit, Prop, Watch } from 'vue-property-decorator';
-
 import './index.less';
+import helloword from '../../components/HelloWorld.vue';
 @Component({
   name: 'Home',
-  components: {}
+  components: {
+    helloword
+  }
 })
 export default class Home extends Vue {
   render() {
-    return <div class="home">home</div>;
+    return (
+      <div class="home">
+        <helloword />
+      </div>
+    );
   }
 }
